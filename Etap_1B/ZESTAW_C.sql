e1b7991a5fe8a9dc72027043b30755140fc45aee
Jakub Nadolny <matryl1@me.com>
2020-03-19 16:12:32 +0100

    Created file for list C

---
+++ Etap_1B/ZESTAW_C.sql
@@ -0,0 +1,18 @@
+--SELECTY
+--1. Zarobek wygenerowany z zamówień przez wybranego pracownika
+
+--2. Który pracownik miał największą różnicę pomiędzy najdroższym a najtańszym produktem
+
+--3. Ilu pracowników ma dane stanowisko
+
+--4. Z którego kraju klient zrobił najdroższe zamówienie
+
+--5. Który pracownik ma najwięcej klientów
+
+
+--CRUDY
+--6. Przeniesienia pracownika do innego biura
+
+--7. Zmiana pracownika odpowiedzialnego za daną grupę klientów
+
+--8. Zmniejszenie limitu kredytowego o 10% dla klientów którzy w wybranym miesiącu zrobili zamówienie na kwotę mniej niż 1000
