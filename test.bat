cb7ff2596efa2781aca10b8b3df3389805f33004
RaBuko <raf.buko@gmail.com>
2020-03-28 19:45:22 +0100

    organize repo

--- test.bat
+++ test.bat
@@ -1,7 +1,7 @@
 
 echo START > timings.txt
 date /t >> timings.txt
-echo @Etap_1B\ZESTAW_A.sql | sqlplus -s C##test1/password1 >> timings.txt
-echo @Etap_1B\ZESTAW_B.sql | sqlplus -s C##test1/password1 >> timings.txt
-echo @Etap_1B\ZESTAW_C.sql | sqlplus -s C##test1/password1 >> timings.txt
+echo @Etap1\A.sql | sqlplus -s C##test1/password1 >> timings.txt
+echo @Etap1\B.sql | sqlplus -s C##test1/password1 >> timings.txt
+echo @Etap1\C.sql | sqlplus -s C##test1/password1 >> timings.txt
 echo END >> timings.txt
