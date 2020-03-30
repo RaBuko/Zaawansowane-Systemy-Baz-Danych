d6533b066260a6aca28f0d4a40ccd6ae289e4f14
Mykhailo25 <248432@student.pwr.edu.pl>
2020-03-30 17:08:40 +0200

    insert danych skryptem INSERT

---
+++ InitDb/INSERT.sql
@@ -0,0 +1,8 @@
+@D:\pwr\Semestr_3\Bazy\ZSBD\InitDb\4_insert_data.sql
+@D:\pwr\Semestr_3\Bazy\ZSBD\InitDb\PRODUCTS.sql
+@D:\pwr\Semestr_3\Bazy\ZSBD\InitDb\OFFICES.sql
+@D:\pwr\Semestr_3\Bazy\ZSBD\InitDb\EMPLOYEES.sql
+@D:\pwr\Semestr_3\Bazy\ZSBD\InitDb\CUSTOMERS.sql
+@D:\pwr\Semestr_3\Bazy\ZSBD\InitDb\PAYMENTS.sql
+@D:\pwr\Semestr_3\Bazy\ZSBD\InitDb\ORDERS.sql
+@D:\pwr\Semestr_3\Bazy\ZSBD\InitDb\ORDERDETAILS.sql
