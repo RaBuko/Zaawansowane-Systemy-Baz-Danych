28a077d532e70129a7d41689bc4c2ca6ff0966de
RaBuko <raf.buko@gmail.com>
2020-04-04 13:34:55 +0200

    testrules

---
+++ Etap2/TestCrud/cruds_measure_time.bat
@@ -0,0 +1,4 @@
+
+echo @Etap1\A.sql | sqlplus -s C##test1/password1
+echo @Etap1\B.sql | sqlplus -s C##test1/password1
+echo @Etap1\C.sql | sqlplus -s C##test1/password1
---
+++ Etap2/TestRules/RULE_2.sql
@@ -0,0 +1,51 @@
+--Crud_2
+-- FLUSH 
+ALTER SYSTEM FLUSH buffer_cache;
+ALTER SYSTEM FLUSH shared_pool;
+
+VARIABLE  START_TIME_CRUD_2 NUMBER;
+EXEC :START_TIME_CRUD_2 := EXTRACT(SECOND FROM SYSTIMESTAMP);
+-- START MIERZENIA CZASU
+
+insert  into payments(customerNumber,checkNumber,paymentDate,amount) 
+values (103,'NEW236335',to_date('2020-02-19','YYYY-MM-DD'),6066.78);
+
+ UPDATE CUSTOMERS CUST
+ SET CUST.CREDITLIMIT = CUST.CREDITLIMIT+100
+ WHERE CUST.CUSTOMERNUMBER = 103;
+
+insert  into payments(customerNumber,checkNumber,paymentDate,amount) 
+values (103,'NEW236336',to_date('2020-02-19','YYYY-MM-DD'),6066.78);
+
+ UPDATE CUSTOMERS CUST
+ SET CUST.CREDITLIMIT = CUST.CREDITLIMIT+100
+ WHERE CUST.CUSTOMERNUMBER = 103;
+
+insert  into payments(customerNumber,checkNumber,paymentDate,amount) 
+values (103,'NEW236337',to_date('2020-02-19','YYYY-MM-DD'),6066.78);
+
+ UPDATE CUSTOMERS CUST
+ SET CUST.CREDITLIMIT = CUST.CREDITLIMIT+100
+ WHERE CUST.CUSTOMERNUMBER = 103;
+ 
+ insert  into payments(customerNumber,checkNumber,paymentDate,amount) 
+values (103,'NEW236338',to_date('2020-02-19','YYYY-MM-DD'),6066.78);
+
+ UPDATE CUSTOMERS CUST
+ SET CUST.CREDITLIMIT = CUST.CREDITLIMIT+100
+ WHERE CUST.CUSTOMERNUMBER = 103;
+ 
+ insert  into payments(customerNumber,checkNumber,paymentDate,amount) 
+values (103,'NEW236339',to_date('2020-02-19','YYYY-MM-DD'),6066.78);
+
+ UPDATE CUSTOMERS CUST
+ SET CUST.CREDITLIMIT = CUST.CREDITLIMIT+100
+ WHERE CUST.CUSTOMERNUMBER = 103;
+
+
+VARIABLE  TIME_FOR_CRUD_2 NUMBER;
+EXEC :TIME_FOR_CRUD_2 := EXTRACT(SECOND FROM SYSTIMESTAMP)-:START_TIME_CRUD_2;
+spool etap2_results.txt append;
+PRINT TIME_FOR_CRUD_2;
+spool off;
+ROLLBACK;
