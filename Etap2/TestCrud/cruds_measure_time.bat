a3104fd70b6357a27b9f1c5eb93c05ecb210b392
RaBuko <raf.buko@gmail.com>
2020-04-04 13:36:42 +0200

    etap2 tests bat

--- Etap2/TestCrud/cruds_measure_time.bat
+++ Etap2/TestCrud/cruds_measure_time.bat
@@ -1,4 +1,4 @@
-
-echo @Etap1\A.sql | sqlplus -s C##test1/password1
-echo @Etap1\B.sql | sqlplus -s C##test1/password1
-echo @Etap1\C.sql | sqlplus -s C##test1/password1
+echo @CRUD_1.sql | sqlplus -s C##test1/password1
+echo @CRUD_2.sql | sqlplus -s C##test1/password1
+echo @CRUD_3.sql | sqlplus -s C##test1/password1
+echo @CRUD_4.sql | sqlplus -s C##test1/password1
--- Etap2/TestRules/rules_measure_time.bat
+++ Etap2/TestRules/rules_measure_time.bat
@@ -1,4 +1,4 @@
-
-echo @Etap1\A.sql | sqlplus -s C##test1/password1
-echo @Etap1\B.sql | sqlplus -s C##test1/password1
-echo @Etap1\C.sql | sqlplus -s C##test1/password1
+echo @RULE_1.sql | sqlplus -s C##test1/password1
+echo @RULE_2.sql | sqlplus -s C##test1/password1
+echo @RULE_3.sql | sqlplus -s C##test1/password1
+echo @RULE_4.sql | sqlplus -s C##test1/password1
