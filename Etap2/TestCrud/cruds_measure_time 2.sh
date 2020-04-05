e96252be8e87f4387163e71c8d210b5e4e181086
Jakub Nadolny <matryl1@me.com>
2020-04-05 16:44:25 +0200

    added shell starter

---
+++ Etap2/TestCrud/cruds_measure_time 2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo @DISABLE_TRIGGERS.sql | sqlplus -s C##test1/password1
+echo @CRUD_1.sql | sqlplus -s C##test1/password1
+echo @CRUD_2.sql | sqlplus -s C##test1/password1
+echo @CRUD_3.sql | sqlplus -s C##test1/password1
+echo @CRUD_4.sql | sqlplus -s C##test1/password1
+echo @ENABLE_TRIGGERS.sql | sqlplus -s C##test1/password1
