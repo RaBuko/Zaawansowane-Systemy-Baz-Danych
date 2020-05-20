89efaae99604e6fc978054ff297a7884e5daa4d5
RaBuko <raf.buko@gmail.com>
2020-05-20 18:19:12 +0200

    drop cities if exists

--- Etap4/2. SchemaExtension.sql
+++ Etap4/2. SchemaExtension.sql
@@ -1,3 +1,13 @@
+declare
+   c int;
+begin
+   select count(*) into c from user_tables where table_name = upper('cities');
+   if c = 1 then
+      execute immediate 'drop table cities';
+   end if;
+end;
+
+
 CREATE TABLE "CITIES"
 (  
 	"CITY" VARCHAR2(50) NOT NULL ENABLE,
